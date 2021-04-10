147f235b6136427ba0a8618b034014f47dac8d7e
Mikey Pinaud <michael.pinaud@gmail.com>
2021-04-10 13:58:43 -0700

    feat(commit): install cz-customizable

---
+++ .cz-config.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  types: [
+    { value: 'feat', name: 'FEAT:       A new feature added' },
+    { value: 'fix', name: 'FIX:        A bug fix' },
+    { value: 'docs', name: 'DOCS:       Documentation only changes' },
+    { value: 'style', name: 'STYLE:      Changes that do not affect the meaning of the code\n              (white-space, formatting, missing semi-colons, etc)' },
+    { value: 'refactor', name: 'REFACTOR:   A code change that neither fixes a bug nor adds a feature' },
+    { value: 'perf', name: 'PERF:       A code change that improves performance' },
+    { value: 'test', name: 'TEST:       Adding missing tests' },
+    { value: 'wip', name: 'WIP:        Work in progress' },
+    { value: 'build', name: 'BUILD:      Changes that affect the build system\n              or external dependencies' },
+    { value: 'ci', name: 'CI:         Changes to our pipleine or CI infrastructure' },
+    { value: 'revert', name: 'REVERT:     Reverted to a commit' },
+  ],
+
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
+  ticketNumberPrefix: 'TICKET-',
+  ticketNumberRegExp: '\\d{1,5}',
+
+  // it needs to match the value for field type. Eg.: 'fix'
+  /*
+  scopeOverrides: {
+    fix: [
+      {name: 'merge'},
+      {name: 'style'},
+      {name: 'e2eTest'},
+      {name: 'unitTest'}
+    ]
+  },
+  */
+  // override the messages, defaults are as follows
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: '\nDenote the SCOPE of this change (optional):',
+    // used if allowCustomScopes is true
+    customScope: 'Denote the SCOPE of this change:',
+    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: 'List any BREAKING CHANGES (optional):\n',
+    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+  },
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
+  // skip any questions you want
+  skipQuestions: ['body'],
+
+  // limit subject length
+  subjectLimit: 100,
+  // breaklineChar: '|', // It is supported for fields body and footer.
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true, // default is false
+};
